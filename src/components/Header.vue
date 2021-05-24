9e8cdb472918aadf45f1a35749b35c4930cd8061
StefanoCeli <stefanopsn91@gmail.com>
2021-05-24 19:58:36 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 70px;
+    background-color: chartreuse;
+}
+</style>
